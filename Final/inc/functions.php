e1bc11fa566852124eba9983537bc057aecb7c8f
unknown <Molly@.(none)>
2014-03-05 23:49:24 -0500

    Start of Scaffold

--- Final/inc/functions.php
+++ Final/inc/functions.php
@@ -1,13 +1,26 @@
 <?php
-   include_once 'password.php';
+   include_once __DIR__ . '/password.php';
    
-   function GetConnention(){
+   function GetConnection(){
    	   global $password;
 	   
        $conn = mysqli_connect('localhost', 'zhangx5', $password, 'zhangx5_db');
 	   return $conn;
    }
    
-   $conn = GetConnection();
-   print_r($conn);
-   
+   function fetch_all(){
+       $conn = GetConnection();
+       $results = $conn->query($sql);
+	   
+       $arr = array(); // real results wants to return
+       
+       while ($row = $results->fetch_assoc()) {
+     		$arr[] = $row;
+       }
+	   
+       $conn->close(); // close as quick as possible
+
+       return $arr;
+   }
+   // $conn = GetConnection();
+   // print_r($conn);
